50cc2668fa220c2318d68201574e1348d34dd22e
Arnaud Schils <arnaud.schils@gmail.com>
2015-10-30 22:27:26 +0100

    testing grid refinement capabilities of deal.II on a simple square.

--- pdetect.cpp
+++ pdetect.cpp
@@ -12,21 +12,39 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+#include <string>
 using namespace dealii;
 
 void first_grid(){
   Triangulation<2> triangulation;
-  GridGenerator::hyper_cube (triangulation);
-  triangulation.refine_global (4);
-  std::ofstream out ("grid-1.eps");
+  GridGenerator::hyper_cube(triangulation);
+  triangulation.refine_global(3);
+
+
+  Triangulation<2>::active_cell_iterator cell, endc;
+
+  unsigned level = 0;
+  unsigned nbr_of_cell_to_refine = 50;
+  cell = triangulation.begin_active(level),
+  endc = triangulation.end();
+
+  for (unsigned i = 0; cell!=endc && i!=nbr_of_cell_to_refine; cell++, i++)
+	  cell->set_refine_flag ();
+
+  triangulation.execute_coarsening_and_refinement ();
+
+
+  std::string fileName = "grid-";
+  fileName = std::to_string(level)+".eps";
+
+  std::ofstream out(fileName);
   GridOut grid_out;
   grid_out.write_eps (triangulation, out);
-  std::cout << "Grid written to grid-1.eps" << std::endl;
+  std::cout << "Grid written to " << fileName << std::endl;
 }
 
 
 int main() {
 	first_grid();
-	std::cout << "Third" << std::endl;
 	return 0;
 }
