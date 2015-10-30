1d672676adfac99fd08ebd986d5d57a4df64d6df
Arnaud Schils <arnaud.schils@gmail.com>
2015-10-30 10:49:56 +0100

    first deal.ii example

--- pdetect.cpp
+++ pdetect.cpp
@@ -1,15 +1,31 @@
 //============================================================================
 // Name        : pdetect.cpp
 // Author      : Arnaud Schils
-// Version     :
-// Copyright   : 
-// Description : Hello World in C++, Ansi-style
 //============================================================================
 
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/tria_accessor.h>
+#include <deal.II/grid/tria_iterator.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/manifold_lib.h>
+#include <deal.II/grid/grid_out.h>
 #include <iostream>
-using namespace std;
+#include <fstream>
+#include <cmath>
+using namespace dealii;
+
+void first_grid (){
+  Triangulation<2> triangulation;
+  GridGenerator::hyper_cube (triangulation);
+  triangulation.refine_global (4);
+  std::ofstream out ("grid-1.eps");
+  GridOut grid_out;
+  grid_out.write_eps (triangulation, out);
+  std::cout << "Grid written to grid-1.eps" << std::endl;
+}
+
 
 int main() {
-	cout << "Second" << endl;
+	first_grid ();
 	return 0;
 }
