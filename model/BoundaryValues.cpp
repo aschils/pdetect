cbacd369a7f6b098c134262a1afedd6acbcbfeac
Arnaud Schils <arnaud.schils@gmail.com>
2015-11-06 21:50:37 +0100

    Fixing wrong indices when specifying boundary conditions.

--- model/BoundaryValues.cpp
+++ model/BoundaryValues.cpp
@@ -9,16 +9,6 @@ template <int dim>
 double BoundaryValues<dim>::value (const Point<dim> &p,
                                    const unsigned int /*component*/) const
 {
-
-
 	double stripWidth = 2.0/3.0; //Strip size is 1/3 of domain size
-
-	//stripCenter.unrolled_to_component_indices(1)
-
-	if(p(1) == -1 && -1+stripWidth <= p(2) && p(2) <= 1-stripWidth)
-		return 1; //1V
-	else
-		return 0;
-
-  //return p.square();
+	return p[1] == -1 && -1+stripWidth <= p[0] && p[0] <= 1-stripWidth;
 }
