1d8879514b52dc6d5bd444a34aadf7767f657ef0
Arnaud Schils <arnaud.schils@gmail.com>
2015-11-06 20:14:03 +0100

    Setting right side of the equation to 0 (i.e. Laplace equation)

--- model/RightHandSide.cpp
+++ model/RightHandSide.cpp
@@ -9,8 +9,9 @@ template <int dim>
 double RightHandSide<dim>::value (const Point<dim> &p,
                                   const unsigned int /*component*/) const
 {
-  double return_value = 0.0;
+  /*double return_value = 0.0;
   for (unsigned int i=0; i<dim; ++i)
     return_value += 4.0 * std::pow(p(i), 4.0);
-  return return_value;
+  return return_value;*/
+  return 0.0;
 }
