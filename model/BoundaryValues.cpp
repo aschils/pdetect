6cd169db1b6b109e89f746a1feb7c699d2cbac6f
slardinois <simon.lardinois@student.uclouvain.be>
2015-11-06 23:00:50 +0100

    Three conductors

--- model/BoundaryValues.cpp
+++ model/BoundaryValues.cpp
@@ -9,6 +9,9 @@ template <int dim>
 double BoundaryValues<dim>::value (const Point<dim> &p,
                                    const unsigned int /*component*/) const
 {
-	double stripWidth = 1.95; //Strip size is 1/3 of domain size
-	return p[1] == -1 && -stripWidth/2 <= p[0] && p[0] <= stripWidth/2;
+	double stripWidth = 1.0/3.0; //Strip size is 1/3 of domain size
+	double middle = -stripWidth/2 <= p[0] && p[0] <= stripWidth/2;
+	double left = p[0] >= -1+stripWidth/2 && p[0] <= -1+3*stripWidth/2;
+	double right = p[0] <= 1-stripWidth/2 && p[0] >= 1-3*stripWidth/2;
+	return p[1] == -1 && (middle || right || left);
 }
