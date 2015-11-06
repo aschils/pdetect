4d2681fc2a44367e371ad77e275eacbed22a3cec
Arnaud Schils <arnaud.schils@gmail.com>
2015-11-06 22:32:46 +0100

    More general.

--- model/BoundaryValues.cpp
+++ model/BoundaryValues.cpp
@@ -9,6 +9,6 @@ template <int dim>
 double BoundaryValues<dim>::value (const Point<dim> &p,
                                    const unsigned int /*component*/) const
 {
-	double stripWidth = 2.0/3.0; //Strip size is 1/3 of domain size
-	return p[1] == -1 && -1+stripWidth <= p[0] && p[0] <= 1-stripWidth;
+	double stripWidth = 1.95; //Strip size is 1/3 of domain size
+	return p[1] == -1 && -stripWidth/2 <= p[0] && p[0] <= stripWidth/2;
 }
