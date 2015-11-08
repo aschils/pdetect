5acf8dac579000b6a2a7f5c5544a513e4d5422b2
Arnaud Schils <arnaud.schils@gmail.com>
2015-11-08 23:01:24 +0100

    Loop trying many number of strips

--- test/potential2D.cpp
+++ test/potential2D.cpp
@@ -10,9 +10,26 @@
 #include "../model/ZeroRightHandSide.hpp"
 
 void test_2D_potentiel(){
+
+	double rect_length_fe = 10;
+
 	ZeroRightHandSide<2> rhs;
-	Rect2DBoundaryValues<2> bv;
-	LaplaceSolver<2> rect_potential_solver(&rhs,&bv);
+
+	for(unsigned nbr_of_strips=0; nbr_of_strips<=10; nbr_of_strips++){
+		Rect2DBoundaryValues<2> bv(nbr_of_strips,1,1,rect_length_fe);
+		LaplaceSolver<2> rect_potential_solver(&rhs,&bv,
+				std::to_string(nbr_of_strips)+".vtk");
+		rect_potential_solver.run();
+	}
+
+	/*
+	Rect2DBoundaryValues<2> bv(1,1,1,rect_length_fe);
+	LaplaceSolver<2> rect_potential_solver(&rhs,&bv, "one_strip.vtk");
 	rect_potential_solver.run();
+
+	//unsigned nbr_of_strip, unsigned strip_length,	unsigned pitch, double rect_length_fe
+	Rect2DBoundaryValues<2> bv2(2,1,1,rect_length_fe);
+	LaplaceSolver<2> rect_potential_solver2(&rhs,&bv2, "two_strip.vtk");
+	rect_potential_solver2.run();*/
 }
 
