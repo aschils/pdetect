1e64f1f798b9021133830b04bbdc52941a1a7090
Arnaud Schils <arnaud.schils@gmail.com>
2015-11-08 23:00:00 +0100

    File where error codes resides (i.e. used when throwing exceptions).

---
+++ model/errors.hpp
@@ -0,0 +1,13 @@
+/*
+ * errors.hpp
+ *
+ *  Created on: 8 nov. 2015
+ *      Author: aschils
+ */
+
+#ifndef __ERRORS__HPP__
+#define __ERRORS__HPP__
+
+#define ZERO_OR_NEGATIVE_DOMAIN_LENGTH_ERROR 1
+
+#endif
