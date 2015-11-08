d4dc33e340210f6c907c81263f0994649617dc7b
Arnaud Schils <arnaud.schils@gmail.com>
2015-11-08 22:59:15 +0100

    Generate boundary values depending on number of strips, strip width and pitch.

--- model/Rect2DBoundaryValues.cpp
+++ model/Rect2DBoundaryValues.cpp
@@ -4,21 +4,76 @@
  *  Created on: 5 nov. 2015
  *      Author: aschils
  */
+template<int dim>
+Rect2DBoundaryValues<dim>::Rect2DBoundaryValues(unsigned nbr_of_strip,
+		unsigned strip_width, unsigned pitch, double rect_length_fe){
 
-template <int dim>
-double Rect2DBoundaryValues<dim>::value (const Point<dim> &p,
-                                   const unsigned int /*component*/) const
-{
+	if(rect_length_fe <= 0)
+		throw ZERO_OR_NEGATIVE_DOMAIN_LENGTH_ERROR;
+
+	this->rect_length_fe = rect_length_fe;
+
+	if(nbr_of_strip == 0 || strip_width == 0){
+		strip_length_fe = 0;
+		pitch_length_fe = 0;
+		strip_pitch_pair_length_fe = 0;
+		return;
+	}
+
+
+	double total_strips_length = nbr_of_strip*strip_width;
+	double total_pitches_length = (nbr_of_strip-1)*pitch;
+	double total_length = total_strips_length+total_pitches_length;
+
+	double total_strips_length_fe =
+			total_strips_length/total_length*rect_length_fe;
+	double total_pitches_length_fe = rect_length_fe-total_strips_length_fe;
+
+	strip_length_fe = total_strips_length_fe/nbr_of_strip;
+	pitch_length_fe = (nbr_of_strip > 1)?
+			total_pitches_length_fe/(nbr_of_strip-1): 0;
+	strip_pitch_pair_length_fe = strip_length_fe+pitch_length_fe;
+
+	std::cout << strip_length_fe << std::endl;
+	std::cout << pitch_length_fe  << std::endl;
+}
+
+template<int dim>
+bool Rect2DBoundaryValues<dim>::is_strip(double component) const {
+
+	//points start at component 0 instead of -rect_length_fe/2
+	double translated_component = rect_length_fe/2+component;
+	unsigned strip_picth_pair_before = (strip_pitch_pair_length_fe > 0)?
+			translated_component/strip_pitch_pair_length_fe: 0;
+
+	std::cout << strip_picth_pair_before << std::endl;
+
+	double delta_from_strip_pitch_pair =
+			translated_component - strip_picth_pair_before*strip_pitch_pair_length_fe;
+	return delta_from_strip_pitch_pair < strip_length_fe;
+}
+
+template<int dim>
+double Rect2DBoundaryValues<dim>::value(const Point<dim> &p,
+		const unsigned int /*component*/) const {
+
+	return p[1] == -1 && is_strip(p[0]);
+
 	//2D case
 	//double stripWidth = 0.5; //Strip size is 1/3 of domain size
 	//return p[1] == -1 && -stripWidth/2 <= p[0] && p[0] <= stripWidth/2;
-	double stripWidth = 2.0/5.0; //Strip size is 1/3 of domain size
+	/*double stripWidth = 2.0 / 5.0; //Strip size is 1/3 of domain size
 
-	double middle  = p[0] >= -2+9*stripWidth/2 && p[0] <= 2-9*stripWidth/2;
-	double m_left  = p[0] >= -2+5*stripWidth/2 && p[0] <= -2+7*stripWidth/2;
-	double m_right = p[0] <= 2-5*stripWidth/2 && p[0] >= 2-7*stripWidth/2;
-	double o_left  = p[0] >= -2+stripWidth/2 && p[0] <= -2+3*stripWidth/2;
-	double o_right = p[0] <= 2-stripWidth/2 && p[0] >= 2-3*stripWidth/2;
-	
-	return p[1] == -1 && (middle || m_right || m_left || o_right || o_left);
+	double middle = p[0] >= -2 + 9 * stripWidth / 2
+			&& p[0] <= 2 - 9 * stripWidth / 2;
+	double m_left = p[0] >= -2 + 5 * stripWidth / 2
+			&& p[0] <= -2 + 7 * stripWidth / 2;
+	double m_right = p[0] <= 2 - 5 * stripWidth / 2
+			&& p[0] >= 2 - 7 * stripWidth / 2;
+	double o_left = p[0] >= -2 + stripWidth / 2
+			&& p[0] <= -2 + 3 * stripWidth / 2;
+	double o_right = p[0] <= 2 - stripWidth / 2
+			&& p[0] >= 2 - 3 * stripWidth / 2;
+
+	return p[1] == -1 && (middle || m_right || m_left || o_right || o_left);*/
 }
--- model/Rect2DBoundaryValues.hpp
+++ model/Rect2DBoundaryValues.hpp
@@ -11,16 +11,39 @@
 #include <deal.II/grid/tria.h>
 #include <deal.II/base/function.h>
 
+#include <math.h>
+
+#include "errors.hpp"
+
 using namespace dealii;
 
-template <int dim>
-class Rect2DBoundaryValues : public Function<dim>
-{
+template<int dim>
+class Rect2DBoundaryValues: public Function<dim> {
 public:
-	Rect2DBoundaryValues () : Function<dim>() {}
-  virtual double value (const Point<dim>   &p,
-                        const unsigned int  component = 0) const;
-};
+
+	/**
+	 * @pre:
+	 * - rect_length is the length of the rectangular finite element
+	 * mathematical domain.
+	 * - rect_length > 0.0
+	 * - strip_width and pitch are values in the domain language
+	 * (i.e. microm,...).
+	 * TODO complete this @pre
+	 */
+	Rect2DBoundaryValues(unsigned nbr_of_strip, unsigned strip_length,
+			unsigned pitch, double rect_length_fe);
+	virtual double value(const Point<dim> &p,
+			const unsigned int component = 0) const;
+
+private:
+	double strip_length_fe = 1.0;
+	double pitch_length_fe = 1.0;
+	double strip_pitch_pair_length_fe = 1.0;
+	double rect_length_fe = 1.0;
+
+	bool is_strip(double length_component) const;
+}
+;
 
 #include "Rect2DBoundaryValues.cpp"
 
