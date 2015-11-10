3ef5ec1e06574dafa389667eac3f716daf6b94b0
Arnaud Schils <arnaud.schils@gmail.com>
2015-11-10 22:20:24 +0100

    Faster alogirthm to set boundary values. Important preconditions are now specified. Possibility to specify potential other than 1V for strips implemented.

--- model/Rect2DBoundaryValues.cpp
+++ model/Rect2DBoundaryValues.cpp
@@ -4,65 +4,86 @@
  *  Created on: 5 nov. 2015
  *      Author: aschils
  */
+
 template<int dim>
 Rect2DBoundaryValues<dim>::Rect2DBoundaryValues(unsigned nbr_of_strip,
-		unsigned strip_width, unsigned pitch, double rect_length_fe){
+		unsigned strip_width, unsigned pitch, double rect_length_fe,
+		double strip_potential){
 
 	if(rect_length_fe <= 0)
 		throw ZERO_OR_NEGATIVE_DOMAIN_LENGTH_ERROR;
 
 	this->rect_length_fe = rect_length_fe;
+	this->strip_potential = strip_potential;
 
 	if(nbr_of_strip == 0 || strip_width == 0){
-		this->strip_length_fe = 0;
-		this->pitch_length_fe = 0;
-		this->strip_pitch_pair_length_fe = 0;
+		strip_length_fe = 0;
+		pitch_length_fe = 0;
+		strip_pitch_pair_length_fe = 0;
 		return;
 	}
 
-
 	double total_strips_length = nbr_of_strip*strip_width;
-	double total_pitches_length = (nbr_of_strip+1)*pitch;
+	double total_pitches_length = nbr_of_strip*pitch;
 	double total_length = total_strips_length + total_pitches_length;
 
 	double total_strips_length_fe =
 			total_strips_length/total_length*rect_length_fe;
 	double total_pitches_length_fe = rect_length_fe - total_strips_length_fe;
 
-	this->strip_length_fe = total_strips_length_fe/nbr_of_strip;
-	this->pitch_length_fe = (nbr_of_strip >= 1)?
-			total_pitches_length_fe/(nbr_of_strip+1) : 0;
-	this->strip_pitch_pair_length_fe = this->strip_length_fe + this->pitch_length_fe;
+	strip_length_fe = total_strips_length_fe/nbr_of_strip;
+	pitch_length_fe = total_pitches_length_fe/nbr_of_strip;
+
+	//Computing these values here for performance reason
+	strip_pitch_pair_length_fe = strip_length_fe + pitch_length_fe;
+	half_pitch_length_fe = pitch_length_fe/2;
 }
 
+/**
+ *
+ * We define a periodic structure as a strip surrounded by half a pitch on
+ * each side. It is the structure repeated on the detector upper surface to
+ * implement a grid of strips/pixels. Examples:
+ *
+ * Detector with one periodic structure:
+ * |  ---  |
+ *
+ * Detector with two periodic structures:
+ * |  ---    ---  |
+ *
+ * @pre:
+ * - if no strip or strip length is 0, half_pitch_length_fe must have been set
+ * to 0 in the constructor.
+ *
+ * @return: true if the there is a strip at position p, false otherwise.
+ */
 template<int dim>
-bool Rect2DBoundaryValues<dim>::is_strip(double pos) const {
+bool Rect2DBoundaryValues<dim>::is_strip(const Point<dim> &p) const {
 
+	if(p[1] != 1)
+		return false;
+
+	double component = p[0];
 	//points start at component 0 instead of -rect_length_fe/2
-	/*double translated_component = this->rect_length_fe/2 + component;
-	unsigned strip_picth_pair_before = (this->strip_pitch_pair_length_fe > 0)?
-			translated_component/this->strip_pitch_pair_length_fe : 0;
-	double delta_from_strip_pitch_pair =
-			translated_component - strip_picth_pair_before*this->strip_pitch_pair_length_fe;
-	return delta_from_strip_pitch_pair < this->strip_length_fe;*/
+	double translated_component = rect_length_fe/2 + component;
+	unsigned nbr_of_prev_periodic_str = (strip_pitch_pair_length_fe > 0)?
+			translated_component/strip_pitch_pair_length_fe : 0;
+	double delta_from_prev_periodic_str = translated_component -
+			nbr_of_prev_periodic_str*strip_pitch_pair_length_fe;
 
-	if(this->strip_length_fe == 0)
-		return 0;
-
-	double i = -this->rect_length_fe/2 + this->pitch_length_fe;
-	double j = -i;
-
-	for(; i <= j; i = i+this->strip_pitch_pair_length_fe){
-		double in_strip = pos >= i && pos <= i+this->strip_length_fe;
-		if(in_strip)
-			return 1;
-	}
-	return 0;
+	if(delta_from_prev_periodic_str <= half_pitch_length_fe)
+		return false;
+	else if(delta_from_prev_periodic_str < strip_length_fe+half_pitch_length_fe)
+		return true;
+	else
+		return false;
 }
 
 template<int dim>
 double Rect2DBoundaryValues<dim>::value(const Point<dim> &p,
 		const unsigned int /*component*/) const {
-
-	return p[1] == 1 && is_strip(p[0]);
+	if(is_strip(p))
+		return strip_potential;
+	else
+		return 0.0;
 }
--- model/Rect2DBoundaryValues.hpp
+++ model/Rect2DBoundaryValues.hpp
@@ -24,14 +24,17 @@ public:
 	/**
 	 * @pre:
 	 * - rect_length_fe is the length of the rectangular finite element
-	 * mathematical domain.
-	 * - rect_length_fe > 0.0
+	 * mathematical domain (i.e. the longest edge of the rectangle).
+	 * - rect_length_fe > 0
 	 * - strip_width and pitch are values in the domain language
-	 * (i.e. microm,...).
-	 * TODO complete this @pre
+	 * (i.e. micrometers,...).
+	 *
+	 * @throw:
+	 * - ZERO_OR_NEGATIVE_DOMAIN_LENGTH_ERROR
+	 *
 	 */
 	Rect2DBoundaryValues(unsigned nbr_of_strip, unsigned strip_length,
-			unsigned pitch, double rect_length_fe);
+			unsigned pitch, double rect_length_fe, double strip_potential);
 	virtual double value(const Point<dim> &p,
 			const unsigned int component = 0) const;
 
@@ -40,8 +43,28 @@ private:
 	double pitch_length_fe = 1.0;
 	double strip_pitch_pair_length_fe = 1.0;
 	double rect_length_fe = 1.0;
+	double half_pitch_length_fe = 1.0;
+	double strip_potential = 1.0;
 
-	bool is_strip(double length_component) const;
+	/**
+	 *
+	 * We define a periodic structure as a strip surrounded by half a pitch on
+	 * each side. It is the structure repeated on the detector upper surface to
+	 * implement a grid of strips/pixels. Examples:
+	 *
+	 * Detector with one periodic structure:
+	 * |  ---  |
+	 *
+	 * Detector with two periodic structures:
+	 * |  ---    ---  |
+	 *
+	 * @pre:
+	 * - if no strip or strip length is 0, half_pitch_length_fe must have been set
+	 * to 0 in the constructor.
+	 *
+	 * @return: true if the there is a strip at position p, false otherwise.
+	 */
+	bool is_strip(const Point<dim> &p) const;
 };
 
 #include "Rect2DBoundaryValues.cpp"
