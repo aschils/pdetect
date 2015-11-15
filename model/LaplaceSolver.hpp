ca3b50c5f15b59ff387a289ee94971b1539eba2e
slardinois <simon.lardinois@student.uclouvain.be>
2015-11-15 23:41:57 +0100

    added periodic boundary values

--- model/LaplaceSolver.hpp
+++ model/LaplaceSolver.hpp
@@ -9,30 +9,32 @@
 #define __LAPLACE_SOLVER_HPP__
 
 #include <deal.II/grid/tria.h>
-#include <deal.II/dofs/dof_handler.h>
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/tria_accessor.h>
 #include <deal.II/grid/tria_iterator.h>
+#include <deal.II/dofs/dof_handler.h>
 #include <deal.II/dofs/dof_accessor.h>
-#include <deal.II/fe/fe_q.h>
 #include <deal.II/dofs/dof_tools.h>
+#include <deal.II/fe/fe_q.h>
 #include <deal.II/fe/fe_values.h>
 #include <deal.II/base/quadrature_lib.h>
 #include <deal.II/base/function.h>
-#include <deal.II/numerics/vector_tools.h>
-#include <deal.II/numerics/matrix_tools.h>
+#include <deal.II/base/logstream.h>
 #include <deal.II/lac/vector.h>
 #include <deal.II/lac/full_matrix.h>
 #include <deal.II/lac/sparse_matrix.h>
 #include <deal.II/lac/dynamic_sparsity_pattern.h>
 #include <deal.II/lac/solver_cg.h>
 #include <deal.II/lac/precondition.h>
+#include <deal.II/lac/constraint_matrix.h>
+#include <deal.II/numerics/vector_tools.h>
+#include <deal.II/numerics/matrix_tools.h>
 #include <deal.II/numerics/data_out.h>
 #include <fstream>
 #include <iostream>
-#include <deal.II/base/logstream.h>
 
 #include "Rect2DBoundaryValues.hpp"
+#include "PeriodicConstraints.hpp"
 
 using namespace dealii;
 
@@ -41,8 +43,10 @@ class LaplaceSolver {
 
 public:
 	LaplaceSolver(double rect_length_fe, double rect_width_fe,
-			const Function<dim> *right_hand_side,
-			Function<dim> *boundary_values, std::string result_file_path);
+					const Function<dim> *right_hand_side,
+					Function<dim> *boundary_values, 
+					PeriodicConstraints<dim> *periodic_constraints,
+					std::string result_file_path);
 	void run();
 
 private:
@@ -58,6 +62,8 @@ private:
 	const Function<dim> *right_hand_side;
 	Function<dim> *boundary_values_fun;
 	std::string result_file_path;
+	PeriodicConstraints<dim> *periodic_constraints;
+	ConstraintMatrix constraints;
 
 	void make_grid();
 	void setup_system();
