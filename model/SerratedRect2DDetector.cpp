b2252064e5bfc508ae4b22cade2296a38cfdf28b
Arnaud Schils <arnaud.schils@gmail.com>
2015-11-16 21:36:35 +0100

    temporary copy of Rect2DDetector to test serrated rectangle geometry

---
+++ model/SerratedRect2DDetector.cpp
@@ -0,0 +1,57 @@
+/*
+ * SerratedRect2DDetector.cpp
+ *
+ *  Created on: 12 nov. 2015
+ *      Author: aschils
+ */
+
+#include "SerratedRect2DDetector.hpp"
+
+/**
+ * Compute the length of the rectangle (detector) in the domain language unit
+ * (microm) depending on the number of strips, the strip length and the pitch.
+ */
+double SerratedRect2DDetector::compute_total_length(){
+	if(nbr_of_strips == 0 || strip_length == 0)
+		return RECT_WIDTH;
+	return nbr_of_strips * (strip_length + pitch);
+}
+
+/**
+ * Rectangle width should always be 300microm. Thus finite element rectangle
+ * width should be adapted depending on the length of the rectangle in microm.
+ */
+double SerratedRect2DDetector::compute_rect_width_fe() {
+	double one_micron_fe = RECT_LENGTH_FE/total_length;
+	return RECT_WIDTH*one_micron_fe;
+}
+
+SerratedRect2DDetector::SerratedRect2DDetector(unsigned nbr_of_strips,
+		unsigned strip_length, unsigned strip_width, unsigned pitch,
+		double strip_potential) {
+
+	this->nbr_of_strips = nbr_of_strips;
+	this->strip_length = strip_length;
+	this->pitch = pitch;
+	this->strip_potential = strip_potential;
+	total_length = compute_total_length();
+	rect_width_fe = compute_rect_width_fe();
+
+	zero_right_hand_side = new ZeroRightHandSide<2>();
+	boundary_val = new Rect2DBoundaryValues<2>(nbr_of_strips, strip_length,
+			pitch, RECT_LENGTH_FE, rect_width_fe, strip_potential);
+	rect_potential_solver = new SerratedLaplaceSolver<2>(RECT_LENGTH_FE, rect_width_fe,
+			zero_right_hand_side, boundary_val,
+			std::to_string(nbr_of_strips) + ".vtk");
+}
+
+SerratedRect2DDetector::~SerratedRect2DDetector() {
+	delete zero_right_hand_side;
+	delete boundary_val;
+	delete rect_potential_solver;
+}
+
+void SerratedRect2DDetector::compute_potential() {
+	rect_potential_solver->run();
+}
+
---
+++ model/SerratedRect2DDetector.hpp
@@ -0,0 +1,38 @@
+/*
+ * SerratedRect2DDetector.hpp
+ *
+ *  Created on: 12 nov. 2015
+ *      Author: aschils
+ */
+
+#ifndef __SERRATED_RECT_2D_DETECTOR_HPP__
+#define __SERRATED_RECT_2D_DETECTOR_HPP__
+
+#include "SerratedLaplaceSolver.hpp"
+#include "ZeroRightHandSide.hpp"
+#include "Rect2DBoundaryValues.hpp"
+
+class SerratedRect2DDetector {
+
+public:
+	SerratedRect2DDetector(unsigned nbr_of_strips, unsigned strip_length,
+			unsigned strip_width, unsigned pitch, double strip_potential);
+	~SerratedRect2DDetector();
+	void compute_potential();
+
+private:
+	unsigned nbr_of_strips, strip_length, strip_width, pitch, total_length = 1;
+	double strip_potential = 1.0;
+	const double RECT_WIDTH = 300.0; //i.e. in domain language (microm,..)
+	const double RECT_LENGTH_FE = 10000.0;
+	double rect_width_fe = 1.0;
+
+	ZeroRightHandSide<2> *zero_right_hand_side;
+	Rect2DBoundaryValues<2> *boundary_val;
+	SerratedLaplaceSolver<2> *rect_potential_solver;
+
+	double compute_total_length();
+	double compute_rect_width_fe();
+};
+
+#endif
