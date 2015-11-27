8fa924ec11d75b7e63bc05b7571764c7f6d76fe8
Arnaud Schils <arnaud.schils@gmail.com>
2015-11-27 23:45:32 +0100

    Added methods to compare double

--- model/Utils.hpp
+++ model/Utils.hpp
@@ -6,12 +6,34 @@
  *      Author: aschils
  */
 
+#ifndef __UTILS_HPP__
+#define __UTILS_HPP__
+
 #include <stdlib.h>
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/tria_accessor.h>
+#include <deal.II/grid/tria_iterator.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/manifold_lib.h>
+#include <deal.II/grid/grid_out.h>
+
+#include <iostream>
+#include <fstream>
 
 class Utils {
 
 public:
-	static bool is_same_double(double a, double b, double epsilon){
-		return abs(a-b) <= epsilon;
+	static bool equals_double(double a, double b, double epsilon) {
+		return abs(a - b) <= epsilon;
+	}
+
+	static bool less_than_or_equals_double(double a, double b, double epsilon){
+		return a < b || equals_double(a, b, epsilon);
+	}
+
+	static bool greater_than_or_equals_double(double a, double b, double epsilon){
+		return a > b || equals_double(a, b, epsilon);
 	}
 };
+
+#endif
