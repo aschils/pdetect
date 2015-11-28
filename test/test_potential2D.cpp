200b06ea3144b6f593dfeaf9e18e9a2a0c71f79b
Arnaud Schils <arnaud.schils@gmail.com>
2015-11-28 22:14:05 +0100

    test function for electric field computation

--- test/test_potential2D.cpp
+++ test/test_potential2D.cpp
@@ -48,60 +48,60 @@ void test_serrated_rect_limit_cases() {
 	double stop_accuracy = 10e-12;
 
 	std::string output_dir = "tests_output/";
-/*
-	//Limit case: hole_width = width
+	/*
+	 //Limit case: hole_width = width
 
-	for (unsigned nbr_of_strips = 0; nbr_of_strips <= max_nbr_of_strips;
-			nbr_of_strips++) {
-		std::string output_file = output_dir + "width_equals_hole_width_"
-				+ std::to_string(nbr_of_strips) + ".vtk";
-		SerratedRect2DDetector srdd(nbr_of_strips, width, strip_length,
-				strip_width, pitch, strip_potential, refine_level, max_iter,
-				stop_accuracy);
-		srdd.compute_potential(output_file);
-	}
+	 for (unsigned nbr_of_strips = 0; nbr_of_strips <= max_nbr_of_strips;
+	 nbr_of_strips++) {
+	 std::string output_file = output_dir + "width_equals_hole_width_"
+	 + std::to_string(nbr_of_strips) + ".vtk";
+	 SerratedRect2DDetector srdd(nbr_of_strips, width, strip_length,
+	 strip_width, pitch, strip_potential, refine_level, max_iter,
+	 stop_accuracy);
+	 srdd.compute_potential(output_file);
+	 }
 
-	//Limit case: strip_length = 0
+	 //Limit case: strip_length = 0
 
-	strip_width = 80;
-	strip_length = 0;
-	for (unsigned nbr_of_strips = 0; nbr_of_strips <= max_nbr_of_strips;
-			nbr_of_strips++) {
-		std::string output_file = output_dir + "strip_length_0_"
-				+ std::to_string(nbr_of_strips) + ".vtk";
-		SerratedRect2DDetector srdd(nbr_of_strips, width, strip_length,
-				strip_width, pitch, strip_potential, refine_level, max_iter,
-				stop_accuracy);
-		srdd.compute_potential(output_file);
-	}
+	 strip_width = 80;
+	 strip_length = 0;
+	 for (unsigned nbr_of_strips = 0; nbr_of_strips <= max_nbr_of_strips;
+	 nbr_of_strips++) {
+	 std::string output_file = output_dir + "strip_length_0_"
+	 + std::to_string(nbr_of_strips) + ".vtk";
+	 SerratedRect2DDetector srdd(nbr_of_strips, width, strip_length,
+	 strip_width, pitch, strip_potential, refine_level, max_iter,
+	 stop_accuracy);
+	 srdd.compute_potential(output_file);
+	 }
 
-	//Limit case: strip_width = 0
-	refine_level = 7;
-	strip_length = 100;
-	strip_width = 0;
-	for (unsigned nbr_of_strips = 0; nbr_of_strips <= max_nbr_of_strips;
-			nbr_of_strips++) {
-		std::string output_file = output_dir + "strip_width_0_"
-				+ std::to_string(nbr_of_strips) + ".vtk";
-		SerratedRect2DDetector srdd(nbr_of_strips, width, strip_length,
-				strip_width, pitch, strip_potential, refine_level, max_iter,
-				stop_accuracy);
-		srdd.compute_potential(output_file);
-	}
+	 //Limit case: strip_width = 0
+	 refine_level = 7;
+	 strip_length = 100;
+	 strip_width = 0;
+	 for (unsigned nbr_of_strips = 0; nbr_of_strips <= max_nbr_of_strips;
+	 nbr_of_strips++) {
+	 std::string output_file = output_dir + "strip_width_0_"
+	 + std::to_string(nbr_of_strips) + ".vtk";
+	 SerratedRect2DDetector srdd(nbr_of_strips, width, strip_length,
+	 strip_width, pitch, strip_potential, refine_level, max_iter,
+	 stop_accuracy);
+	 srdd.compute_potential(output_file);
+	 }
 
-	//pitch = 0
+	 //pitch = 0
 
-	strip_width = 80;
-	pitch = 0;
-	for (unsigned nbr_of_strips = 0; nbr_of_strips <= max_nbr_of_strips;
-			nbr_of_strips++) {
-		std::string output_file = output_dir + "pitch_0_"
-				+ std::to_string(nbr_of_strips) + ".vtk";
-		SerratedRect2DDetector srdd(nbr_of_strips, width, strip_length,
-				strip_width, pitch, strip_potential, refine_level, max_iter,
-				stop_accuracy);
-		srdd.compute_potential(output_file);
-	}*/
+	 strip_width = 80;
+	 pitch = 0;
+	 for (unsigned nbr_of_strips = 0; nbr_of_strips <= max_nbr_of_strips;
+	 nbr_of_strips++) {
+	 std::string output_file = output_dir + "pitch_0_"
+	 + std::to_string(nbr_of_strips) + ".vtk";
+	 SerratedRect2DDetector srdd(nbr_of_strips, width, strip_length,
+	 strip_width, pitch, strip_potential, refine_level, max_iter,
+	 stop_accuracy);
+	 srdd.compute_potential(output_file);
+	 }*/
 
 	for (unsigned nbr_of_strips = 0; nbr_of_strips <= 3; nbr_of_strips++) {
 		for (unsigned strip_length = 0; strip_length <= 50; strip_length +=
@@ -113,8 +113,8 @@ void test_serrated_rect_limit_cases() {
 					SerratedRect2DDetector srdd(nbr_of_strips, width,
 							strip_length, strip_width, pitch, strip_potential,
 							refine_level, max_iter, stop_accuracy);
-					std::string output_file = output_dir +
-							srdd.params_to_string()+ ".vtk";
+					std::string output_file = output_dir
+							+ srdd.params_to_string() + ".vtk";
 					srdd.compute_potential(output_file);
 
 				}
@@ -125,5 +125,28 @@ void test_serrated_rect_limit_cases() {
 	//Limit case: hole_width = width && strip_length = 0
 
 	//Limit case: hole_width = width && strip_length = 0
+}
+
+void test_electric_field() {
+	ZeroRightHandSide<2> rhs;
+	double strip_potential = 1;
+	unsigned strip_length = 100;
+	unsigned strip_width = 50;
+	unsigned pitch = 100;
+	unsigned refine_level = 5;
+	unsigned max_iter = 10000;
+	double stop_accuracy = 10e-12;
+	unsigned nbr_of_strips = 3;
+	unsigned width = 300;
+
+	std::string output_dir = "tests_output/";
+
+	std::string output_file = output_dir + "pitch_"
+			+ std::to_string(pitch) + "_" + std::to_string(nbr_of_strips)
+			+ ".vtk";
+	SerratedRect2DDetector srdd(nbr_of_strips, width, strip_length, strip_width, pitch,
+			strip_potential, refine_level, max_iter, stop_accuracy);
+	srdd.compute_potential(output_file);
+	srdd.compute_electric_field(output_dir+"EE.vtk");
 }
 
