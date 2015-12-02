82001015231510d69b4084b858aea142f5c1472d
Arnaud Schils <arnaud.schils@gmail.com>
2015-12-02 23:02:52 +0100

    Compute electric field now returns a DataOut object

--- model/Detector2D.hpp
+++ model/Detector2D.hpp
@@ -8,6 +8,8 @@
 #ifndef __DETECTOR2D_HPP__
 #define __DETECTOR2D_HPP__
 
+#include <deal.II/numerics/data_out.h>
+
 #include "Solution.hpp"
 #include "Gradient.hpp"
 
@@ -16,7 +18,7 @@ class Detector2D {
 public:
 	virtual Solution<2> compute_potential() = 0;
 	virtual Solution<2> compute_weighting_potential() = 0;
-	virtual void compute_electric_field(std::string gradient_file_path) = 0;
+	virtual DataOut<2> compute_electric_field() = 0;
 	virtual ~Detector2D(){}
 };
 
