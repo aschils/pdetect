0e45a58d9832c1c3526ab45bb12129271eb6c12e
Arnaud Schils <arnaud.schils@gmail.com>
2015-12-06 23:53:24 +0100

    Function to prind the Solution in a nice format in the terminal. Dataout related code is commented because not running on a Linux Ubuntu config for unknown reasons.

--- model/Solution.hpp
+++ model/Solution.hpp
@@ -15,8 +15,8 @@
 #include <deal.II/numerics/data_out.h>
 #include <deal.II/numerics/data_out_dof_data.h>
 
+#include "TensorUtils.hpp"
 #include "Utils.hpp"
-#include "VectorUtils.hpp"
 
 using namespace dealii;
 
@@ -42,43 +42,56 @@ class Solution {
 
 public:
 
-	//These two structures contain data already available in coord_and_data,
-	//but it is useful to keep them as such to easily output vtk graph file
-	//using deal.ii DataOut class.
-	DataOut<dim> fun_drawer;
-	DataOut<dim> derivatives_drawer;
 	std::vector<std::pair<std::vector<double>, SolutionData<dim> > > coord_and_data;
 
 	Solution() {
 	}
 
+	void set_fun_drawer(DataOut<dim> fun_drawer){
+		this->fun_drawer = fun_drawer;
+	}
+
+	void set_derivatives_drawer(DataOut<dim> derivatives_drawer){
+		this->derivatives_drawer = derivatives_drawer;
+	}
+
 	void draw_vtk_graph_fun(std::string output_file) {
 		std::ofstream output(output_file);
-		fun_drawer.write_vtk(output);
+		//fun_drawer.write_vtk(output);
 	}
 
 	void draw_vtk_graph_derivatives(std::string output_file) {
 		std::ofstream output(output_file);
-		derivatives_drawer.write_vtk(output);
+		//derivatives_drawer.write_vtk(output);
 	}
 
 	void sort_by_coord() {
 		VectorUtils::sort_by_coord<dim, SolutionData<dim> >(coord_and_data);
 	}
 
-	void print() {
+	void print(){
 
-		for (unsigned i = 0; i < coord_and_data.size(); i++) {
+		for(unsigned i=0; i<coord_and_data.size(); i++){
 			std::cout << "[coord: (";
 			VectorUtils::print_vec_components(coord_and_data[i].first);
 			std::cout << "), function: " << coord_and_data[i].second.fun;
 			std::cout << " gradient: (";
 			VectorUtils::print_tensor_components<dim>(
 					coord_and_data[i].second.gradient);
+			std::cout << ") hessian: (";
+			VectorUtils::print_tensor_components<dim>(
+								coord_and_data[i].second.hessian);
 			std::cout << ")] ";
 		}
 		std::cout << std::endl;
 	}
+
+private:
+	//These two structures contain data already available in coord_and_data,
+	//but it is useful to keep them as such to easily output vtk graph file
+	//using deal.ii DataOut class.
+	//DataOut<dim> fun_drawer;
+	//DataOut<dim> derivatives_drawer;
 };
 
 #endif
