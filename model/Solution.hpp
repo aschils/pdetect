1a8077ee828ae3ce76683115c7f42fe54c9515b2
Arnaud Schils <arnaud.schils@gmail.com>
2015-12-06 14:57:49 +0100

    Receive just DataOut objects in constructor. Then set the values, gradient, etc after: more efficient because we can directly write in the variable of the Solution class, i.e. we don't have to copy a big data structure afterwards.

--- model/Solution.hpp
+++ model/Solution.hpp
@@ -20,20 +20,21 @@
 
 using namespace dealii;
 
+template<unsigned dim>
 class SolutionData {
 public:
-	double solution;
-	std::vector<double> gradient;
-	std::vector<double> seconde_derivatives;
+	double fun;
+	Tensor<1, dim> gradient;
+	Tensor<2, dim> hessian;
 
-	SolutionData(double solution, std::vector<double> gradient,
-			std::vector<double> seconde_derivatives) {
-		this->solution = solution;
+	SolutionData(double fun, Tensor<1, dim> gradient, Tensor<2, dim> hessian) {
+		this->fun = fun;
 		this->gradient = gradient;
-		this->seconde_derivatives = seconde_derivatives;
+		this->hessian = hessian;
 	}
 
-	SolutionData(){}
+	SolutionData() {
+	}
 };
 
 template<int dim>
@@ -41,91 +42,39 @@ class Solution {
 
 public:
 
-	DoFHandler<dim> *dof_handler;
-	std::vector<std::pair<std::vector<double>, SolutionData> > coord_and_data;
-	//These two structures contain data already available in coord_and_data,
-	//but it is useful to keep them as such to easily output vtk graph file
-	//using deal.ii DataOut class.
-	Vector<double> solution;
-	DataOut<dim> derivatives_data_container;
+	std::vector<std::pair<std::vector<double>, SolutionData<dim> > > coord_and_data;
 
 	Solution() {
 	}
 
-	Solution(Vector<double> solution_vec, Derivatives<dim> &derivatives,
-			DoFHandler<dim> *dof_handler) {
-
-		this->dof_handler = dof_handler;
-		this->solution = solution_vec;
-
-		derivatives_data_container.attach_dof_handler(*dof_handler);
-		derivatives_data_container.add_data_vector(solution_vec, derivatives);
-		derivatives_data_container.build_patches();
-		//Triangulation<2>::active_cell_iterator first = derivatives_data_container.first_cell();
-		//first->vertex(0).
-
-		/*
-		for (; cell != endc; ++cell) {
-				for (unsigned int v = 0; v < GeometryInfo<2>::vertices_per_cell; ++v) {
-
-					Point<2> p = cell->vertex(v);
-		*/
-
-
-		std::stringstream derivatives_stream;
-		//TODO find better way to retrieve data from DataPostProcessor
-		//std::stringstream stream_temp;
-		//derivatives_data_container.write_deal_II_intermediate(stream_temp);
-		//std::cout << stream_temp.str() << std::endl;
-
-		derivatives_data_container.write_gnuplot(derivatives_stream);
-		std::vector<std::pair<std::vector<double>, std::vector<double> > >
-		coord_and_derivatives;
-		Utils::parse_gnuplot<dim>(derivatives_stream, dim + dim * dim,
-				coord_and_derivatives);
-
-		//std::cout << derivatives_stream.str() << std::endl;
-
-		coord_and_data.resize(coord_and_derivatives.size());
-
-		for(unsigned i=0; i< coord_and_derivatives.size(); i++){
-			std::pair<std::vector<double>, SolutionData>
-			coord_and_data_one_point;
-			coord_and_data_one_point.first = coord_and_derivatives[i].first;
-
-			std::vector<double> gradient(dim);
-			std::vector<double> second_derivatives(dim*dim);
-
-			for(unsigned j=0; j<dim; j++)
-				gradient[j] = coord_and_derivatives[i].second[j];
-			for(unsigned j=0; j<(dim*dim); j++)
-				second_derivatives[j] = coord_and_derivatives[i].second[dim+j];
-
-			//std::cout << "i: " << i << " size " << solution.size()  << std::endl;
-
-			SolutionData data_one_point(solution[i], gradient, second_derivatives);
-			coord_and_data_one_point.second = data_one_point;
-			coord_and_data[i] = coord_and_data_one_point;
-		}
+	void set_fun_drawer(DataOut<dim> fun_drawer){
+		this->fun_drawer = fun_drawer;
 	}
 
-	void draw_vtk_graph_solution(std::string output_file) {
-		DataOut<2> data_out;
-		data_out.attach_dof_handler(*(this->dof_handler));
-		data_out.add_data_vector(solution, "solution");
-		data_out.build_patches();
+	void set_derivatives_drawer(DataOut<dim> derivatives_drawer){
+		this->derivatives_drawer = derivatives_drawer;
+	}
+
+	void draw_vtk_graph_fun(std::string output_file) {
 		std::ofstream output(output_file);
-		data_out.write_vtk(output);
+		fun_drawer.write_vtk(output);
 	}
 
 	void draw_vtk_graph_derivatives(std::string output_file) {
 		std::ofstream output(output_file);
-		derivatives_data_container.write_vtk(output);
+		derivatives_drawer.write_vtk(output);
 	}
 
 	void sort_by_coord() {
-		VectorUtils::sort_by_coord<dim, SolutionData>(coord_and_data);
+		VectorUtils::sort_by_coord<dim, SolutionData<dim> >(coord_and_data);
 	}
+
+private:
+	//These two structures contain data already available in coord_and_data,
+	//but it is useful to keep them as such to easily output vtk graph file
+	//using deal.ii DataOut class.
+	DataOut<dim> fun_drawer;
+	DataOut<dim> derivatives_drawer;
 };
 
 #endif
