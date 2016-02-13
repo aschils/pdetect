ae2eea9851da931521e153f8c3b3451df3f8b5e6
Arnaud Schils <arnaud.schils@gmail.com>
2016-02-14 00:08:05 +0100

    some tests

--- pdetect.cpp
+++ pdetect.cpp
@@ -12,13 +12,15 @@ int main(int argc, char* argv[]) {
 	//test_serrated_rect_limit_cases();
 	//test_electric_field();
 	//test_weighting_potential();
-	test_various();
-	test_mid_circle_rect2D_det();
-	test_mid_rect_rect_2D_det();
+	//test_various();
+	//test_mid_circle_rect2D_det();
+	//test_mid_rect_rect_2D_det();
 
 	//test_rectangle_with_circular_hole();
 	//test_rectangle_width_rectangular_holes();
 	//test_serrated_rectangle_gaussian();
+
+	test_electrode_current();
 
 	//Cli cli(argc, argv);
 	//cli.parse_cmd();
--- test/test_electrode_current.hpp
+++ test/test_electrode_current.hpp
@@ -9,4 +9,48 @@
 
 #include "../model/ElectrodeCurrent.hpp"
 
+void test_electrode_current() {
+
+	double strip_potential = 1;
+	unsigned nbr_of_strips = 1;
+	unsigned width = 300;
+	unsigned strip_length = 100;
+	unsigned strip_width = 50;
+	unsigned half_pitch = 50;
+	unsigned refine_level = 5;
+	unsigned max_iter = 10000;
+	double stop_accuracy = 10e-12;
+
+	//std::string output_dir = "tests_electrode_current";
+	//Utils::create_directory_if_not_exists(output_dir);
+
+	SerratedRect2DDetector srdd(nbr_of_strips, width, strip_length, strip_width,
+			half_pitch, strip_potential, refine_level, max_iter, stop_accuracy);
+	srdd.compute();
+	srdd.compute_weight();
+
+	Solution<2> potential = srdd.get_solution_potential();
+	Solution<2> weight_potential = srdd.get_solution_weight_potential();
+	MyGeometryInfo *geo_info = srdd.get_geometry_info();
+
+	std::vector<
+			std::pair<typename DoFHandler<2>::active_cell_iterator,
+					std::vector<Tensor<1, 2> > > > electric_field =
+			srdd.get_electric_field();
+
+	std::vector<
+			std::pair<typename DoFHandler<2>::active_cell_iterator,
+					std::vector<Tensor<1, 2> > > > electric_field_weight =
+			srdd.get_electric_field_weight();
+
+
+	Point<2> p1(0,300);
+	Point<2> p2(10, 300);
+	Segment seg(p1,p2);
+
+	Line particle_traj(5, 0.0);
+
+	ElectrodeCurrent<2> ec(geo_info, &potential, &weight_potential, &electric_field,
+			&electric_field_weight, &particle_traj, refine_level);
+}
 
