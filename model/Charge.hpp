95a674c188e33ce49fda2cb6c38f28e542058c68
Arnaud Schils <arnaud.schils@gmail.com>
2016-02-14 13:48:44 +0100

    Abstracting a charge (electron or hole)

---
+++ model/Charge.hpp
@@ -0,0 +1,59 @@
+/*
+ * Charge.hpp
+ *
+ *  Created on: 14 févr. 2016
+ *      Author: aschils
+ */
+
+#pragma once
+
+#define TYPE_SILICIUM 1
+
+class Charge {
+
+public:
+	//static const double MASS;
+
+	double get_mobility() {
+		return mobility;
+	}
+
+protected:
+	double mobility;
+	double SILICIUM_MOBILITY;
+	unsigned type;
+	Charge(unsigned type) {
+		this->type = type;
+	}
+
+	void set_mobility() {
+		switch (type) {
+		case TYPE_SILICIUM:
+			mobility = SILICIUM_MOBILITY;
+			break;
+		default:
+			mobility = SILICIUM_MOBILITY;
+		}
+	}
+};
+
+class Hole: public Charge {
+
+	//static const double MASS = 1;
+public:
+	Hole(unsigned type) :
+			Charge(type) {
+		this->SILICIUM_MOBILITY = 4.5e10; //(microm)^2/(Vs)
+		set_mobility();
+	}
+};
+
+class Electron: public Charge {
+
+public:
+	Electron(unsigned type) :
+			Charge(type) {
+		this->SILICIUM_MOBILITY = 1.35e11; //(microm)^2/(Vs)
+		set_mobility();
+	}
+};
