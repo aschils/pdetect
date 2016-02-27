c53b8637b2afed122ccf523da68560295ebc430a
Arnaud Schils <arnaud.schils@gmail.com>
2016-02-27 18:50:40 +0100

    convert seconds to nanoseconds before writing results to file

--- model/ResultsOut.hpp
+++ model/ResultsOut.hpp
@@ -15,7 +15,20 @@ public:
 
 	static void write_current_vs_time(std::string file,
 			std::vector<std::pair<double, double> > &current_vs_time){
-		Utils::write_vector_of_pair<double, double>(file, current_vs_time);
+
+		//Convert seconds in nanoseconds
+
+		std::vector<std::pair<double, double> >
+		current_vs_nanosec(current_vs_time.size());
+
+		for(unsigned i=0; i<current_vs_time.size(); i++){
+			std::pair<double, double> p(current_vs_time[i].first*1e9,
+					current_vs_time[i].second);
+			current_vs_nanosec[i] = p;
+		}
+
+		Utils::write_vector_of_pair<double, double>(file, current_vs_nanosec,
+				true, "TIME[ns],", "Itot[uA],");
 	}
 
 };
