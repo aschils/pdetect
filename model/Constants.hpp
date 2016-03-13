d3dc18d929da7935b26d3a7e6e55e9aa5972e1c5
Arnaud Schils <arnaud.schils@gmail.com>
2016-03-13 23:26:02 +0100

    Additionnal constants, speed of light,...

--- model/Constants.hpp
+++ model/Constants.hpp
@@ -12,6 +12,13 @@ const double ELECTRON_CHARGE = 1.602176e-13; // microCoulomb
 const double GAS_CONSTANT = 8.3144598;// J/(K mol)
 const double ATMOSPHERIC_PRESSURE = 1.01325e5; // Pascal
 const double MOLAR_MASS_HELIUM = 0.0040026; // kg/mol
+const double LIGHT_SPEED = 299792458e6; //microm/s
 
-#define TYPE_SILICIUM 1
+#define TYPE_SILICON 1
 #define TYPE_HELIUM 2
+
+//Energy per length deposited by particle passing through matter
+const double HELIUM_DE_DX = 0.032; //eV/microm
+
+//Average energy to produce an ion pair
+const double HELIUM_ION_PAIR_E = 24.5; //eV
