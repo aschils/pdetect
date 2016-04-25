76a5a85bc4dfe9138a2d77bfae49f2456e5f6f67
Arnaud Schils <arnaud.schils@gmail.com>
2016-04-25 21:20:28 +0200

    do not plot ratio at points where some data are unavailable

--- plots/plot_pdetect_vs_wf_current.R
+++ plots/plot_pdetect_vs_wf_current.R
@@ -22,8 +22,12 @@ for(dir_name in dir_names){
   #remove useless columns
   wf_I_vs_t = wf_I_vs_t[,c(time_col_name ,"Itot.uA..")]
 
-  min_time = min(c(min(pdetect_I_vs_t[time_col_name ]), min(wf_I_vs_t[time_col_name ])))
-  max_time = max(c(max(pdetect_I_vs_t[time_col_name ]), max(wf_I_vs_t[time_col_name ])))
+  min_time_pdetect = min(pdetect_I_vs_t[time_col_name ])
+  min_time_wf = min(wf_I_vs_t[time_col_name ])
+  min_time = min(c(min_time_pdetect, min_time_wf))
+  max_time_pdetect = max(pdetect_I_vs_t[time_col_name ])
+  max_time_wf = max(wf_I_vs_t[time_col_name ])
+  max_time = max(c(max_time_pdetect, max_time_wf))
   xlim = c(min_time, max_time)
 
   min_current = min(c(min(pdetect_I_vs_t[current_col_name]), min(wf_I_vs_t[current_col_name])))
@@ -40,13 +44,16 @@ for(dir_name in dir_names){
   pdet_spline = splinefun(pdetect_I_vs_t)
   wf_spline = splinefun(wf_I_vs_t)
 
-  t = seq(from=min_time, to=max_time, by=(max_time-min_time)/nbr_of_pt_spline)
+  min_time_ratio = max(c(min_time_pdetect, min_time_wf))
+  max_time_ratio = min(c(max_time_pdetect,max_time_wf))
+
+  t = seq(from=min_time_ratio, to=max_time_ratio, by=(max_time_ratio-min_time_ratio)/nbr_of_pt_spline)
 
   pdet_y = pdet_spline(t)
   wf_y = wf_spline(t)
 
   print(dir_name)
-  print(my_mse(pdet_y, wf_y))
+  #print(my_mse(pdet_y, wf_y))
 
   #Plot ratio
   ratio = (pdet_y-1)/(wf_y-1)
